edb6d4251e598e5a7dc6718d0a3445df892ff0f2
Tony Kalinan <yerraguditonykalinan@gmail.com>
2025-11-14 13:23:35 +0530

    added tasks component and input name of user on select on user component and emitted generic output

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, Input } from '@angular/core';
 import { Header } from './header/header';
 import { User } from './user/user';
 import { DUMMY_USERS } from './dummy-users';
+import { Tasks } from './tasks/tasks';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, User],
+  imports: [Header, User, Tasks],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('learningAngular');
   users = DUMMY_USERS;
+  user = ''
 
   onSelectUser(id: string) {
-    console.log('selected User: ' + id);
+    // console.log('selected User: ' + id);
+    const userselected = this.users.find(user => user.id === id);
+    this.user = userselected!.name;
   }
 }
