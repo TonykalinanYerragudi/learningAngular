bbf63e6f9fa1fb19d90d01f4d102407ed8b5a066
Tony Kalinan <yerraguditonykalinan@gmail.com>
2025-11-14 13:26:07 +0530

    user component change

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -15,7 +15,7 @@ export class User {
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
 
-   @Output() select = new EventEmitter();
+  @Output() select = new EventEmitter<string>();
 
   // using output function
   //select = output<string>();
